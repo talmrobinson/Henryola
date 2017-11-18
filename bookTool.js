27e0a3d3bba92d4ff4c7a305da9a67cef7e18f34
Glitch (bitter-tiger) <none>
2017-11-18 20:14:44 +0000

    Checkpoint

--- bookTool.js
+++ bookTool.js
@@ -1,15 +1,11 @@
 var request = require('request');
 
-function bookTool(endRes, data){
-  //console.log(today+offset);
-  request.post("http://calendar.library.ucsc.edu/rooms_acc.php?gid=302&d=2017-11-" +(today+offset)+ "&cap=0", function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      console.log("ERROR from Mch >:(");
-      endRes.writeHead(200, {"Content-Type": "text/plain"});
-      endRes.write("Hello Book");
-      endRes.end();
-    }
-    
-      
+function bookMch(endRes, data){
+  request.( "http://calendar.library.ucsc.edu/process_roombookings.php?m=booking_full"+data, function (error, response, data) {
+    console.log(error);
+    console.log(response);
+    console.log(data);
   });
 }
+
+exports.bookMch = bookMch
