16111f1a7b1782ee52ae5769ea6a9d31d6ca41b7
Kevin Crum <kevinbaileycrum@gmail.com>
2017-12-07 11:53:44 -0800

    removed redundant function
    Tal & Kevin

--- bookTool.js
+++ bookTool.js
@@ -1,4 +1,4 @@
-     
+         
 
 var Horseman = require("node-horseman");
 var horseman = new Horseman();
@@ -31,39 +31,11 @@ function bookMch(endRes, params){
   }, params );
   
   endRes.writeHead(200, {"Content-Type": "text/plain"});
-  endRes.write("Hello Book");
+  endRes.write("Booking Room");
   endRes.end();
   console.log(params);
   
   
 }
 exports.bookMch = bookMch
-
-
-function do_booking(params) {
-  // this is the AJAX HTTP POST CALL that tries to connect to the McH server and book the room
-  // $ means im using the jQuery library and ajax() is their function for making http calls
-  // most different 
-  $.ajax({
-  type: "POST", //type of HTTP call.. there are more just google HTTP call types
-    //headers: headers1, I removed this line because it was throwing lots of errors when we try to use the false origin headers
-  url: "http://calendar.library.ucsc.edu/process_roombookings.php?m=booking_full", // the server to connect to 
-  crossDomain: true, // this just signifies that the origin of the http call is coming from a different server than the destination
-  data: jQuery.param(params), // this is the data to be sent with the call, jquery.param() formats 'parameters' into something nice
-  success: function(data) {
-			/*error*/
-			if (data.status === 1) {
-              console.log("Error Email");
-				errorAlert(data.msg);
-
-				/*Mediation, Tentative or Confirmed*/
-			} else if (data.status === 2) {
-                console.log("Success!");
-			}
-		}, // this is the callback function that should be called after the request is sent and processed by the server 
-           // 'data' is the information that is sent back to us by the server (nothing happens cause the request is rejected as of now)
-  dataType: "json" // the format of the returned file 
-});
-	return false; // return value is unimportant as of right now
-}
 
